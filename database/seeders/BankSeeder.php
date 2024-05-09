fc48a8db903ca0964f9fe38cc2f4af8cb18dca29
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-09 19:13:32 -0300

    Bank model, config, seeder

---
+++ database/seeders/BankSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bank;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BankSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Bank::insert(collect(config('banks'))->map(fn ($bank) => [
+            'name' => $bank['name'],
+            'code' => $bank['code'],
+            'updated_at' => now(),
+            'created_at' => now(),
+        ])->toArray());
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             TransactionSeeder::class,
+            BankSeeder::class,
         ]);
     }
 }
