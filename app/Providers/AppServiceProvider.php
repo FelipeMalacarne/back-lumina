0d3d3acf87999ca1f9a30e5e756bf1b60f0e1812
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-10 11:48:26 -0300

    Debugsql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +23,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        if (env('APP_DEBUG_SQL', false)) {
+            DB::listen(function ($query) {
+                $sql = $query->sql;
+                $bindings = $query->bindings;
+                $time = $query->time;
+                $connectionName = $query->connectionName;
+                $sql = str_replace('?', "'%s'", $sql);
+                $fullSql = vsprintf($sql, $bindings);
+                Log::debug("[$connectionName] $fullSql ($time ms)");
+            });
+        }
     }
 }
