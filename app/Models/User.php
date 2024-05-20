024156500e518afb038f339cb91f74bd95b1db4b
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-20 09:20:35 -0300

    Email verification enable

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Enums\ProjectType;
 use App\Enums\Role;
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable, HasUuids;
 
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::get('/user', fn (Request $request) => UserResource::make($request->user()));
     Route::get('/projects', [ProjectsController::class, 'index'])->name('projects.index');
     Route::get('/accounts', [AccountsController::class, 'index'])->name('accounts.index');
