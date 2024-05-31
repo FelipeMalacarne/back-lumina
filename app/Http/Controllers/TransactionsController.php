e39a1d2791bc5f35cc596dd9c6ea883dd46c4c68
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-31 18:07:04 -0300

    #3 Transactions Controller and resource

---
+++ app/Http/Controllers/TransactionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TransactionResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TransactionsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $transactions = $request->user()->defaultProject->transactions()->paginate();
+
+        return TransactionResource::collection($transactions);
+    }
+
+    public function store(Request $request)
+    {
+        $transaction = $request->user()->transactions()->create($request->all());
+
+        return response()->json($transaction, Response::HTTP_CREATED);
+    }
+
+    public function show(Request $request, string $transaction)
+    {
+        return response()->json($request->user()->transactions()->findOrFail($transaction), Response::HTTP_OK);
+    }
+
+    public function update(Request $request, string $transaction)
+    {
+        $transaction = $request->user()->transactions()->findOrFail($transaction);
+        $transaction->update($request->all());
+
+        return response()->json($transaction, Response::HTTP_OK);
+    }
+
+    public function destroy(Request $request, string $transaction)
+    {
+        $transaction = $request->user()->transactions()->findOrFail($transaction);
+        $transaction->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->formatted_amount,
+            'date_posted' => $this->date_posted->toDateString(),
+            'memo' => $this->memo,
+            'account_id' => $this->account_id,
+            'created_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
